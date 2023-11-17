17badbd47be179e36aeb9a18de7b9186aef40cc7
gowthamkish <gowthamkish@outlook.com>
2023-11-17 13:23:02 +0530

    backend + frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // index.js
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const userRoute = require("./routes/userRoutes");
@@ -17,16 +18,30 @@ app.use(
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
 
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+
+// Serve frontend
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set env PRODUCTION"));
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT} `);
 });
 
 // Export the Express API
-module.exports = app;
+// module.exports = app;
